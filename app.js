c7641a4da89c2a0c150a8503ca313674e985a39c
yandkbuildcon <157867169+yandkbuildcon@users.noreply.github.com>
2024-01-31 00:47:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.use(express.json());
 app.use('/uploads',express.static('uploads'));
 
 
+//-----------------------------------------------------------
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 // =================================== Enable CORS for all routes website
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
