f0f647c127fcb093a0b4aedcecb99b0e6105bbef
Harshit Kumar Sahu <https://github.com/Tysondgn>
2024-01-31 00:37:28 +0530

    cors gpt

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/uploads',express.static('uploads'));
 
-//=================================== Enable CORS for all routes website
+// =================================== Enable CORS for all routes website
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
 //   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -26,6 +26,27 @@ app.use('/uploads',express.static('uploads'));
 //     next();
 //   }
 // });
+
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://127.0.0.1:5500', 'http://www.yandkbuildcon.com'];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+
 
 // // Custom CORS configuration
 // app.use((req, res, next) => {
