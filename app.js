49ceada93109d2c80e8700755fc44fa3d9c040b5
yandkbuildcon <157867169+yandkbuildcon@users.noreply.github.com>
2024-02-01 09:31:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://127.0.0.1:5500', 'http://www.yandkbuildcon.com'];
+  const allowedOrigins = ['http://127.0.0.1:5000', 'http://www.yandkbuildcon.com'];
 
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
